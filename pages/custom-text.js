b130c3f4b7a47544815e234adf0ae541763b7ca7
Dryha Andriy <andrii.dr@fernir.co>
2025-02-18 15:48:55 +0200

    disallowed the Enter button in the custom text

--- pages/custom-text.js
+++ pages/custom-text.js
@@ -1,23 +1,28 @@
 import { useState } from 'react';
-
 import Head from 'next/head';
-
 import { MainLayout } from '../components/MainLayout/MainLayout';
 import TypeTest from '../components/TypeTest/TypeTest';
 
 export default function CustomText() {
     const [text, setText] = useState('');
-    const [showTypeTest, setShowTypeTest] = useState(false)
+    const [showTypeTest, setShowTypeTest] = useState(false);
 
     const onChangeHandler = (event) => {
         setText(event.target.value);
-    }
+    };
 
     const startTyping = () => {
-        if(text){
+        if (text) {
             setShowTypeTest(true);
         }
-    }
+    };
+
+    // Запрещаем ввод Enter
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    };
 
     return (
         <MainLayout>
@@ -34,12 +39,12 @@ export default function CustomText() {
                                 placeholder='Введите свой текст сюда'
                                 value={text}
                                 onChange={onChangeHandler}
+                                onKeyDown={handleKeyDown}  // Добавляем обработчик
                             />
                             <button onClick={startTyping}>Печатать</button>
                         </div>
                     )
-
             }
         </MainLayout>
-    )
+    );
 }
